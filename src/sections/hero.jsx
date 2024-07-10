50080dca3bce18d00314d003f424257753c7aa4f
Marius Hummel <hummelmarius87@gmail.com>
2024-07-10 15:30:34 +0200

    inital commit

---
+++ src/sections/hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+function Hero() {
+  return (
+    <section id='hero' className='Hero'>
+      <div className='content'>
+        <h1 className='Name'>
+          Hummel <br />
+          Marius
+        </h1>
+        <h2 className='Description'>
+          Need a new Website? <br />
+          Let's create something unique!
+        </h2>
+      </div>
+      <div className='circle' />
+    </section>
+  );
+}
+
+export default Hero;
