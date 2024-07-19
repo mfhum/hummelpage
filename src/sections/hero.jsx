4cd3148bf25cea8736626dadab74da8750a2f53e
Marius Hummel <hummelmarius87@gmail.com>
2024-07-19 13:33:50 +0200

    minor changes

--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -4,16 +4,14 @@ function Hero() {
     <section id='hero' className='Hero'>
       <div className='content'>
         <div className='svg name'>
-          <div className='heroHover'>
-            <svg className='svgHero' width='750' height='217' viewBox='0 0 609 217' fill='none' xmlns='http://www.w3.org/2000/svg'>
-              <path
-                d='M224.284 23.5458C395.589 -62.4085 757.913 122.074 540.027 188.114C267.669 270.664 -189.52 148.591 87.4209 28.0192C207.83 -24.4031 397.197 28.3511 453.625 53.5164'
-                stroke='black'
-                strokeWidth='2'
-              />
-            </svg>
-            <h1 className='nameText heroTitle'>Hummel</h1>
-          </div>
+          <svg className='svgHero' width='750' height='217' viewBox='0 0 609 217' fill='none' xmlns='http://www.w3.org/2000/svg'>
+            <path
+              d='M224.284 23.5458C395.589 -62.4085 757.913 122.074 540.027 188.114C267.669 270.664 -189.52 148.591 87.4209 28.0192C207.83 -24.4031 397.197 28.3511 453.625 53.5164'
+              stroke='black'
+              strokeWidth='2'
+            />
+          </svg>
+          <h1 className='nameText heroTitle'>Hummel</h1>
           <h1 className='heroTitle'>Marius</h1>
           <div className='titleWrapper'></div>
         </div>
